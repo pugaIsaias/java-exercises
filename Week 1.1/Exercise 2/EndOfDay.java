881518f017a454de5eb0227f20ec5ee6c09004ee
Puga.isaias <64232550+pugaIsaias@users.noreply.github.com>
2021-03-16 19:45:40 -0600

    week 1.1 exercise 2 completed

--- Week 1.1/Exercise 2/EndOfDay.java
+++ Week 1.1/Exercise 2/EndOfDay.java
@@ -1,10 +1,18 @@
+import java.util.Calendar;
+
 public class EndOfDay{
     
     /** Cree un programa que al correrlo le diga cuantos segundos 
     quedan para que se termine el día. */
-    
+
     public static void main(String[] args)
     {
-        System.out.println("Hello");
+        Calendar rightNow = Calendar.getInstance();
+        rightNow.add(Calendar.DAY_OF_MONTH,1);
+        rightNow.set(Calendar.HOUR_OF_DAY, 0);
+        rightNow.set(Calendar.MINUTE, 0);
+        rightNow.set(Calendar.SECOND, 0);
+        rightNow.set(Calendar.MILLISECOND, 0);
+        System.out.println(rightNow.getTimeInMillis()-System.currentTimeMillis());
     }
 }
