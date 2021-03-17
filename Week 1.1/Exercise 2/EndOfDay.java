ef72fbc86f0211e7db8198bca086c92b538f10d3
Puga.isaias <64232550+pugaIsaias@users.noreply.github.com>
2021-03-16 19:17:33 -0600

    initial setup for week 1.1 exercise 2

---
+++ Week 1.1/Exercise 2/EndOfDay.java
@@ -0,0 +1,10 @@
+public class EndOfDay{
+    
+    /** Cree un programa que al correrlo le diga cuantos segundos 
+    quedan para que se termine el día. */
+    
+    public static void main(String[] args)
+    {
+        System.out.println("Hello");
+    }
+}
