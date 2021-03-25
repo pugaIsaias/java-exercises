e60aeae174d8b71130b2443a46050b0e81de262e
Puga.isaias <64232550+pugaIsaias@users.noreply.github.com>
2021-03-24 21:48:39 -0600

    calculateWinner find if winner positions

--- Week 2.1/1/TikTakToeGame.java
+++ Week 2.1/1/TikTakToeGame.java
@@ -14,10 +14,18 @@ public class TikTakToeGame {
         int initialPlayer = (random.nextFloat()<0.5)?0:1;
         System.out.println("Comenzará el jugador "+(initialPlayer+1)+" : "+playersName[initialPlayer]);
         
-        Board.setPos(1,'X');
-        Board.setPos(9,'O');
+        Board.setPos(1,'x');
+        Board.setPos(2,'x');
+        Board.setPos(3,'x');
         Board.printBoard();
+        char winner = Game.calculateWinner(Board.getPos());
+        System.out.println("The winner is: "+winner);
         Board.reset();
+        Board.setPos(1,'x');
+        Board.setPos(2,'o');
+        Board.setPos(3,'x');
+        winner = Game.calculateWinner(Board.getPos());
+        System.out.println("The winner is: "+winner);
         Board.printBoard();
 
         input.close();
@@ -25,7 +33,29 @@ public class TikTakToeGame {
 
     static class Game {
 
+        static char calculateWinner(char[] squares){
+            final int[][] LINES = {
+                {0, 1, 2},
+                {3, 4, 5},
+                {6, 7, 8},
+                {0, 3, 6},
+                {1, 4, 7},
+                {2, 5, 8},
+                {0, 4, 8},
+                {2, 4, 6},
+            };
 
+            for (int i = 0; i < LINES.length; i++) {
+                int a = LINES[i][0];
+                int b = LINES[i][1];
+                int c = LINES[i][2];
+                if (squares[a] == squares[b] && squares[a] == squares[c]) {
+                  return squares[a];
+                }
+              }
+
+            return ' ';
+        }
     }
 
     static class Board {
@@ -54,8 +84,12 @@ public class TikTakToeGame {
 
         static public void reset(){
             for (int i=1; i<10; i++){
-                setPos(i,' ');
+                setPos(i,(char) (i+ '0'));
             }
+        }
+
+        static public char[] getPos(){
+            return pos;
         }
     }
 
