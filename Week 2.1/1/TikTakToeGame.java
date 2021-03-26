b554a21aaf6ed7bfe7d9cf03b8c7aacac087c1b6
Puga.isaias <64232550+pugaIsaias@users.noreply.github.com>
2021-03-25 21:53:50 -0600

    able to instanciate classes

--- Week 2.1/1/TikTakToeGame.java
+++ Week 2.1/1/TikTakToeGame.java
@@ -13,84 +13,99 @@ public class TikTakToeGame {
         Random random = new Random();
         int initialPlayer = (random.nextFloat()<0.5)?0:1;
         System.out.println("Comenzará el jugador "+(initialPlayer+1)+" : "+playersName[initialPlayer]);
-        
-        Board.setPos(1,'x');
-        Board.setPos(2,'x');
-        Board.setPos(3,'x');
-        Board.printBoard();
-        char winner = Game.calculateWinner(Board.getPos());
+        Board board = new Board();
+        board.setPos(1,'x');
+        board.setPos(2,'x');
+        board.setPos(3,'x');
+        board.printBoard();
+        char winner = Game.calculateWinner(board.getPos());
         System.out.println("The winner is: "+winner);
-        Board.reset();
-        Board.setPos(1,'x');
-        Board.setPos(2,'o');
-        Board.setPos(3,'x');
-        winner = Game.calculateWinner(Board.getPos());
+        board.reset();
+        board.setPos(1,'x');
+        board.setPos(2,'o');
+        board.setPos(3,'x');
+        winner = Game.calculateWinner(board.getPos());
         System.out.println("The winner is: "+winner);
-        Board.printBoard();
+        board.printBoard();
 
         input.close();
     }
 
-    static class Game {
+   
+    
+}
 
-        static char calculateWinner(char[] squares){
-            final int[][] LINES = {
-                {0, 1, 2},
-                {3, 4, 5},
-                {6, 7, 8},
-                {0, 3, 6},
-                {1, 4, 7},
-                {2, 5, 8},
-                {0, 4, 8},
-                {2, 4, 6},
-            };
+class Game {
 
-            for (int i = 0; i < LINES.length; i++) {
-                int a = LINES[i][0];
-                int b = LINES[i][1];
-                int c = LINES[i][2];
-                if (squares[a] == squares[b] && squares[a] == squares[c]) {
-                  return squares[a];
-                }
-              }
+    private int turn = 0;
+    private Board board;
 
-            return ' ';
-        }
+    //constructor
+    public Game(){
+        board = new Board();
     }
 
-    static class Board {
+    void isTurnOf(){
+        System.out.println((turn));
+    }
 
-        static char[] pos = {
-            '1','2','3',
-            '4','5','6',
-            '7','8','9'
+    char calculateWinner(char[] squares){
+        final int[][] LINES = {
+            {0, 1, 2},
+            {3, 4, 5},
+            {6, 7, 8},
+            {0, 3, 6},
+            {1, 4, 7},
+            {2, 5, 8},
+            {0, 4, 8},
+            {2, 4, 6},
         };
 
-        
-        static public void printBoard(){
-            System.out.println();
-            System.out.println(" "+pos[0]+" | "+pos[1]+" | "+pos[2]);
-            System.out.println("---+---+---");
-            System.out.println(" "+pos[3]+" | "+pos[4]+" | "+pos[5]);
-            System.out.println("---+---+---");
-            System.out.println(" "+pos[6]+" | "+pos[7]+" | "+pos[8]); 
-            System.out.println();
-        }
-
-        static public void setPos(int there, char move){
-            if (0<there&&there<10)
-                pos[there-1] = move;
-        }
-
-        static public void reset(){
-            for (int i=1; i<10; i++){
-                setPos(i,(char) (i+ '0'));
+        for (int i = 0; i < LINES.length; i++) {
+            int a = LINES[i][0];
+            int b = LINES[i][1];
+            int c = LINES[i][2];
+            if (squares[a] == squares[b] && squares[a] == squares[c]) {
+              return squares[a];
             }
-        }
+          }
 
-        static public char[] getPos(){
-            return pos;
+        return ' ';
+    }
+}
+
+
+class Board {
+
+    char[] pos = {
+        '1','2','3',
+        '4','5','6',
+        '7','8','9'
+    };
+
+    
+    public void printBoard(){
+        System.out.println();
+        System.out.println(" "+pos[0]+" | "+pos[1]+" | "+pos[2]);
+        System.out.println("---+---+---");
+        System.out.println(" "+pos[3]+" | "+pos[4]+" | "+pos[5]);
+        System.out.println("---+---+---");
+        System.out.println(" "+pos[6]+" | "+pos[7]+" | "+pos[8]); 
+        System.out.println();
+    }
+
+    public void setPos(int there, char move){
+        if (0<there&&there<10)
+            pos[there-1] = move;
+    }
+
+    public void reset(){
+        for (int i=1; i<10; i++){
+            setPos(i,(char) (i+ '0'));
         }
     }
 
+    public char[] getPos(){
+        return pos;
+    }
 }
