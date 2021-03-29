dd45d4e2200fd71c345fc1fe05159c4ba22056e9
Puga.isaias <64232550+pugaIsaias@users.noreply.github.com>
2021-03-29 17:05:36 -0600

    TaskList tests successful

--- Week 2.1/2/Tester.java
+++ Week 2.1/2/Tester.java
@@ -5,6 +5,7 @@ public class Tester {
     public static void main(String[] args) {
         //Test for Task
         System.out.println("/////Start Task tests/////");
+        
         //Instance with msg
         System.out.println("Testing instance Task with msg");
         Task item1 = new Task("Hello first instance with just msg");
@@ -31,7 +32,27 @@ public class Tester {
         System.out.println(item1.getTaskDate());
         System.out.println(item3.getTaskDate());
         System.out.println("-----------");
+        
         System.out.println("/////Finish Task tests/////");
+        
+        System.out.println("/////Start TaskList tests/////");
 
+        //Init with name
+        System.out.println("Test instance with name");
+        TaskList tasksList1 = new TaskList("First list");
+        tasksList1.printList();
+        
+        //Init with name and task
+        System.out.println("Test instance with name and task");
+        TaskList tasksList2 = new TaskList("Second list", item1);
+        tasksList2.printList();
+
+        System.out.println("-----------");
+        System.out.println("Test of adding items to a List");
+        tasksList1.addTask(item1);
+        tasksList1.addTask(item2);
+        tasksList1.addTask(item3);
+        tasksList1.printList();
+        System.out.println("/////Finish TaskList tests/////");
     }
 }
