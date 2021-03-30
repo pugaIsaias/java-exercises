d399ceeba25a5bddf92a114a31ae4bbcccbc405f
Puga.isaias <64232550+pugaIsaias@users.noreply.github.com>
2021-03-29 18:53:32 -0600

    add testing

---
+++ Week 2.1/4/Testing.java
@@ -0,0 +1,8 @@
+public class Testing {
+   public static void main(String[] args) {
+       Slist<Integer> list = new Slist<Integer>();
+       list.addFirst(1);
+       list.addFirst(0);
+       list.addLast(2);
+   } 
+}
